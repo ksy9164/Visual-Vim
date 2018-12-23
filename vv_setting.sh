f54b3a661d7ec83d93163cafb5c6bff3be6cc826
Seongyoung Kang <ksy9164@naver.com>
2018-12-24 00:00:29 +0900

    Add Colorful vvc command

--- vv_setting.sh
+++ vv_setting.sh
@@ -1,16 +1,24 @@
 export TERM="xterm-256color"
 
+# color define
+RED='\033[1;31m'
+NC='\033[0m'
+BLU='\033[0;34m'
+YEL='\033[1;33m'
+CY='\033[0;36m'
+LCY='\033[1;36m'
+
 vvc(){
     if [[ ($1 == "ls" ) ||  ($1 == "list") ]]; then
-        echo "Visual-Vim Session list "
-        echo "------------------------"
+        echo -e "${LCY}Visual-Vim Session list$(tput sgr 0) "
+        echo -e "------------------------"
         session_list_txt=$(ls ~/Visual-Vim/session_log/)
         session_list=$(echo "$session_list_txt" | cut -d "." -f1)
 
         for i in $session_list ; do
             session_item=$(cat ~/Visual-Vim/session_log/$i.txt | cut -d "!" -f2 | head -1)
-            echo "Session number $i"
-            echo "VV component is $session_item"
+            echo -e "${YEL}Session number ${RED}$i${NC}"
+            echo -e "${LCY}VV component :${CY}$session_item"
             echo ""
         done
         return 1
@@ -19,7 +27,7 @@ vvc(){
     elif [[ ($1 == "kill" ) ||  ($1 == "k") ]]; then
         rm ~/Visual-Vim/session_log/$2.txt
         tmux kill-session -t $2
-        echo "kill VV session $2"
+        echo -e "${YEL}kill VV session $2"
     fi
 }
 vv(){
@@ -30,7 +38,7 @@ vv(){
     #create session
     if [ -e ~/Visual-Vim/session_log/$sc.txt ]
     then
-        echo " $sc screen exist error!!"
+        echo -e "${RED}$sc screen exist error!!"
     else
         touch ~/Visual-Vim/session_log/$sc.txt
 
@@ -41,7 +49,7 @@ vv(){
         tmux resize-pane -t 2 -y 11
         tmux resize-pane -t 2 -x 115
 
-        echo "$sc$screen_id ! $1 !" >> ~/Visual-Vim/session_log/$sc.txt
+        echo "$sc$screen_id ! $@ !" >> ~/Visual-Vim/session_log/$sc.txt
 
         # tmux send-keys -t 0 "data=$sc_term$("tty")" C-j
 
