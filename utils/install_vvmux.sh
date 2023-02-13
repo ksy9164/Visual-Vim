6e0b81bf09865e76eb0bda876c13a36868a5dc4c
Seongyoung-Kang <seongyk3@uci.edu>
2023-02-13 12:51:14 -0800

    Update : vvmux for tmux2.1 install script has been updated

--- utils/install_vvmux.sh
+++ utils/install_vvmux.sh
@@ -1,10 +1,10 @@
 git clone https://github.com/tmux/tmux.git
 cd tmux
-sudo apt install gcc
-sudo apt install libevent-dev
-sudo apt install autotools-dev
-sudo apt install automake
-sudo apt-get install ncurses-dev
-sudo apt install apt
+sudo apt install -y gcc
+sudo apt install -y libevent-dev
+sudo apt install -y autotools-dev
+sudo apt install -y automake
+sudo apt install -y ncurses-dev
+git checkout 2.1
 sh autogen.sh
 ./configure && make
