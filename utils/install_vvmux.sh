5a96add73ba3ae8ad76ef3104cfe645e7b94bd9c
Seongyoung-Kang <seongyk3@uci.edu>
2023-03-01 17:51:37 -0800

    Update: Supporting tmux 2.1 for ubuntu22.04

--- utils/install_vvmux.sh
+++ utils/install_vvmux.sh
@@ -1,10 +1,8 @@
-git clone https://github.com/tmux/tmux.git
-cd tmux
+tar xvzf tmux_2.1.orig.tar.gz
+cd tmux-2.1
 sudo apt install -y gcc
 sudo apt install -y libevent-dev
 sudo apt install -y autotools-dev
 sudo apt install -y automake
 sudo apt install -y ncurses-dev
-git checkout 2.1
-sh autogen.sh
 ./configure && make
